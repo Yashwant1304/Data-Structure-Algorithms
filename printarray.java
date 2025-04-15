083ac4a6e005fc2481c81b48f323a9f9a7778128
Yashwant1304 <yksinghemail@gmail.com>
2025-04-15 17:55:35 +0530

    add printarray

---
+++ printarray.java
@@ -0,0 +1,8 @@
+public class printarray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        for(int x : arr){
+            System.out.print(x+" ");
+        }
+    }
+}
